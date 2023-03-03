bd2b8fd7d4c1f32952026582728d9d804884cdf1
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-03-03 12:43:23 +0100

    finish components

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,7 +1,27 @@
 import PropTypes from 'prop-types';
 
-export const TransactionHistory = ({ transactions }) => {
+export const TransactionHistory = ({ items }) => {
+   return (
+     <table>
+       <thead>
+         <tr>
+           <th>Type</th>
+           <th>Amount</th>
+           <th>Currency</th>
+         </tr>
+       </thead>
 
+       <tbody>
+         {items.map(({ id, type, amount, currency }) => (
+           <tr key = {id}>
+             <td>{type}</td>
+             <td>{amount}</td>
+             <td>{currency}</td>
+           </tr>
+         ))}
+       </tbody>
+     </table>
+   );
 };
 
 TransactionHistory.propTypes = {
@@ -10,3 +30,4 @@ TransactionHistory.propTypes = {
    amount: PropTypes.string.isRequired,
    currency: PropTypes.string.isRequired
 }
+
